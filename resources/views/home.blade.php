aab611faf80114f30dc55cdff7b88fb6641c6cfb
Dean Reight F. Billedo <113150846+deanbilledo@users.noreply.github.com>
2025-03-14 19:48:09 +0800

    made the call to action
    did it

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
     <section class="bg-gray-100 rounded-lg my-6 p-4 lg:p-8 relative overflow-hidden z-0">
         <div class="flex flex-col lg:flex-row justify-between items-center">
             <div class="z-10 mb-8 lg:mb-0 lg:w-1/2 text-left">
-                <p class="text-gray-600 mb-2">No need to worry,</p>
-                <h2 class="text-4xl font-bold mb-4">We Provide Grooming and Vet Checks</h2>
+                <p class="text-gray-600 mb-2">Go book an appointment,</p>
+                <h2 class="text-4xl font-bold mb-4">Find the shop you need</h2>
                
                 <div class="relative">
                     <div class="relative flex items-center">
@@ -20,7 +20,7 @@
                     <input 
                         type="text" 
                         id="location-search" 
-                        placeholder="Pet Services Near You" 
+                        placeholder="Search for shops or services" 
                             class="w-full px-12 py-2 border rounded-full text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     >
                         <svg class="h-5 w-5 text-gray-400 absolute right-3 cursor-pointer hover:text-blue-500 transition-colors duration-200" 
